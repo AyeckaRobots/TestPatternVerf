9ad716e8e84f4b790faee1512c3c7d444b62ba7c
ayeckarobots <Robots@ayecka.com>
2025-03-25 10:50:45 +0200

    Feat: also checks if the frame counter is increasing

--- read_packets.py
+++ read_packets.py
@@ -20,7 +20,7 @@ def check_test_pattern(df, tp_start_index: int) -> None:
 
 capture: pyshark.LiveCapture = pyshark.LiveCapture(interface="eth0") #display_filter='ip.src == 192.168.10.102'
 capture.set_debug()
-capture.sniff(packet_count=40)
+capture.sniff(packet_count=1000)
 tp_start = 19
 
 first_capture = True
@@ -30,19 +30,27 @@ for packet in capture:
         
         df = packet["dvb-s2_bb"].df
         new_df_counter = int("".join(df[:11].split(':')) , 16)
+        new_modeadapt_counter = int(packet["dvb-s2_modeadapt"].frameno)
         print(f'{new_df_counter}  {datetime.now()}  {len(df)//3 + 1}  {int(packet["dvb-s2_bb"].dfl)//8}')
         check_test_pattern(df, tp_start)
         
         if first_capture:
             current_df_counter = new_df_counter
+            current_modeadapt_counter = new_modeadapt_counter
             first_capture = False
         
         elif not first_capture:
-            if new_df_counter == current_df_counter + 1:
+            if new_df_counter == (current_df_counter + 1) % 4294967296:  # might as well include it ;-; (when counter reches its max and resets to 0)
                 pass
             else:
                 print("tp counter of order", new_df_counter, current_df_counter)
             current_df_counter = new_df_counter
+
+            if new_modeadapt_counter == (current_modeadapt_counter + 1) % 256:
+                pass
+            else:
+                print("frame number of order", new_modeadapt_counter, current_modeadapt_counter)
+            current_modeadapt_counter = new_modeadapt_counter
 
 
 
@@ -54,7 +62,7 @@ VERIFY:
 
 packet counter in modeadapt layer
 
-tp counter
-tp payload size (12e4)
+tp counter - done
+tp payload size (12e4) - done
 tp payload - done
 """
